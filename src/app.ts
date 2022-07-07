382b63d9d27b5aeb1f0a570876064f59e1dac78f
Luiz Gustavo Farias <lgsfarias.dev@gmail.com>
2022-07-06 22:19:17 -0300

    initial commit

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express, { Request,Response} from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req: Request, res:Response) => {
+  res.status(200).json({
+    message: 'Welcome to starfighters API'
+  })
+})
+
+export default app
